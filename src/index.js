5c9e62f5dfa34e23651f675025f8d11c25d98206
Zach Kelling <z@zeekay.io>
2017-09-26 08:59:00 -0500

    Export wait

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import Bot          from './Bot';
+import {Bot, wait}  from './Bot';
 import Buttons      from './Buttons';
 import Elements     from './Elements';
 import QuickReplies from './QuickReplies';
 
 export default Bot
 export {
-  Bot, Buttons, Elements, QuickReplies
+  Bot, Buttons, Elements, QuickReplies, wait
 }
